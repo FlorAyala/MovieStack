b9cc363cbea52e6305d8be1b8ccaa968ca0c3d73
Florencia Ayala <flor.ayala30@gmail.com>
2024-04-02 20:13:05 -0300

    se termino el index y movies html y agregaron las crads con js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let crearCards = obj => `
         <h3 class= "font-medium pl-4 text-lg ">${obj.title}</h3>
         <h4 class= "pl-4 ">${obj.tagline}</h4>
         <p class="pl-4 pb-[10px]"> ${obj.overview.slice(0,40)}...</p>
-        <a  href="" class= "bg-black  text-white p-2 w-2/4 text-center">Ver mas</a>
+        <a  href="" class= "bg-black  text-white p-2 w-2/4 text-center rounded-xl ">Ver mas</a>
     
 `
 //console.log(crearCards(data));
@@ -17,10 +17,11 @@ let crearCards = obj => `
 //renderisar las cards en el hmtl para que aparezca en pantalla//
 // con un construcutor DocumentFragment creo un nuevo obj que una vez utilizado se borra
 let fragmento = new DocumentFragment()
+
 for (const iterator of data) {
     let aux = document.createElement("div")
+    aux.className =" flex flex-col  w-3/4 gap-3 p-3  rounded-lg md:w-1/3 lg:w-1/4 hover:scale-110 duration-700 mt-3  shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)]"
     aux.innerHTML +=crearCards(iterator)
-    aux.className ="flex flex-col  w-3/4 border-black border-solid border-2 gap-3 p-3   rounded-lg md:w-1/3 lg:w-1/4 "
     fragmento.appendChild(aux)
 }
 contenedorCards.appendChild(fragmento)//le agrego al contenedo que trae desde el HTML al fragmento como nodo/hijo
